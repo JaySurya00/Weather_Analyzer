25ca4cba378f51b038937acd10e7d6dfe0c03984
Jay Surya <raj.jaysurya04@gmail.com>
2024-10-20 12:41:07 +0530

    fixed weather summary formula

--- services/dailyWeather.js
+++ services/dailyWeather.js
@@ -1,24 +1,19 @@
 const dailyWeatherData = require('../data/dailyWeatherData.js');
 
 const dailyWeather = (weatherDataArray) => {
-    // Loop over each weather data point in the array
     weatherDataArray.forEach(weatherData => {
         const { city } = weatherData;
 
-        // Get the current date as yyyy-mm-dd (to avoid using time)
         const currentDate = new Date().toISOString().split('T')[0];
 
-        // Initialize the city's data if it doesn't exist
         if (!dailyWeatherData[city]) {
             dailyWeatherData[city] = {};
         }
 
-        // Initialize the array for the current date if it doesn't exist
         if (!dailyWeatherData[city][currentDate]) {
             dailyWeatherData[city][currentDate] = [];
         }
 
-        // Push the weather data to the array for the current date
         dailyWeatherData[city][currentDate].push(weatherData);
     });
 };
